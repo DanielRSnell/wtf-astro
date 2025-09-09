e6dfd4fcd87ee4e5b23008d7100523fd6f325ee7
Daniel Snell <broke@Mac.lan>
2025-09-09 14:02:22 -0400

    change from server to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
